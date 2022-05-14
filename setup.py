81cab0d70ec95b62a5e1b74afcfc88e797c79ab0
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 18:15:30 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-date-table-widget',
@@ -7,8 +15,10 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Date Table Widget',
+    description='PyQt table widget for showing date',
     url='https://github.com/yjg30737/pyqt-date-table-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
